a43f9d06eb510601d172b73f153fc3fc82503eea
Christian Lyngbye <chrilyng@gmail.com>
2015-03-24 13:44:32 +0100

    added incrementing nicknames

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var users = 0;
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
+  ++users;
+  nick = 'user'+users;
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
+  io.emit('new user', nick); 
+  io.emit('chat message', '** connected **');
 });
 
 http.listen(3000, function(){
